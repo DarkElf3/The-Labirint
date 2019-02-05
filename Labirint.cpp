3373164066ec9b1c87c511ef79d17bf8cf8680ff
DarkElf3 <47346392+DarkElf3@users.noreply.github.com>
2019-02-05 12:18:13 +0300

    Добавлена информация о вершинах, начальный осмотр
    Добавлена информация о вершинах графа: где на карте, посещалась ли.
    Выделил функцию AddGraphNode для добавления в граф вершины с информацией
    Осматриваем начальную позицию с учетом границ лабиринта, добавляем все видимые вершины

--- Labirint.cpp
+++ Labirint.cpp
@@ -11,28 +11,47 @@ using namespace std;
 **/
 static string *PArray; // Входная карта лабиринта
 static vector<int> Maze; // Карта с номерами вершин графа
-static vector< vector<int> > Graph; // Граф лабиринта
+
 static int R; // number of rows.
 static int C; // number of columns.
 static int A; // number of rounds between the time the alarm countdown is activated and the time the alarm goes off.
+
+struct TNodeInfo { // Тип информации о вершинах графа
+	bool visited;
+	int Row, Col;
+};
+
+static vector< vector<int> > Graph; // Граф лабиринта
+static vector<TNodeInfo> GraphInfo; // Информация о вершинах графа
+
+void AddGraphNode(int Row, int Col, int Index, int *GIndex)
+// Добавляет вершину с информацией в граф
+// Отмечает номер вершины на карте
+{
+	vector<int> Node;
+	Graph.push_back(Node);
+
+	*GIndex = Graph.size() - 1;
+	Maze[Index] = *GIndex;
+
+	TNodeInfo NodeInfo = { false, Row, Col };
+	GraphInfo.push_back(NodeInfo);
+}
 
 void AddNode(int Row, int Col)
 // Добавляет вершину в граф. Связывает ребрами с соседними.
 // Если соседей нет в графе, то они добавляются рекурсивно.
 {
-	int Index = Row * R + Col;
-	if (Maze[Index] != -1) return;
-	vector<int> Node;
-	Graph.push_back(Node);
-	int GIndex = Graph.size() - 1;
-	Maze[Index] = GIndex;
+	if (Row < 0 || Row > R - 1 || Col < 0 || Col > C - 1) return;
+	int Index = Row * C + Col;
+	if (Maze[Index] != -1) return; // Вершина уже есть в графе
+	int GIndex;
+	AddGraphNode(Row, Col, Index, &GIndex);
 
 	if (Col > 0 && PArray[Row][Col - 1] == '.') { // Слева есть путь
 		int LeftGIndex = Maze[Index - 1];
 		if (LeftGIndex == -1) { // Поля слева нет в графе, добавляем
-			Graph.push_back(Node);
-			LeftGIndex = Graph.size() - 1;
-			Maze[Index - 1] = LeftGIndex;
+			AddGraphNode(Row, Col - 1, Index - 1, &LeftGIndex);
 		}
 		Graph[GIndex].push_back(LeftGIndex); // Делаем ребро 
 		Graph[LeftGIndex].push_back(GIndex);
@@ -41,9 +60,7 @@ void AddNode(int Row, int Col)
 	if (Col < (C - 1) && PArray[Row][Col + 1] == '.') { // Справа есть путь
 		int RightGIndex = Maze[Index + 1];
 		if (RightGIndex == -1) { // Поля справа нет в графе, добавляем
-			Graph.push_back(Node);
-			RightGIndex = Graph.size() - 1;
-			Maze[Index - 1] = RightGIndex;
+			AddGraphNode(Row, Col + 1, Index + 1, &RightGIndex);
 		}
 		Graph[GIndex].push_back(RightGIndex); // Делаем ребро 
 		Graph[RightGIndex].push_back(GIndex);
@@ -52,9 +69,7 @@ void AddNode(int Row, int Col)
 	if (Row > 0 && PArray[Row - 1][Col] == '.') { // Сверху есть путь
 		int UpGIndex = Maze[Index - C];
 		if (UpGIndex == -1) { // Поля сверху нет в графе, добавляем
-			Graph.push_back(Node);
-			UpGIndex = Graph.size() - 1;
-			Maze[Index - C] = UpGIndex;
+			AddGraphNode(Row - 1, Col, Index - C, &UpGIndex);
 		}
 		Graph[GIndex].push_back(UpGIndex); // Делаем ребро 
 		Graph[UpGIndex].push_back(GIndex);
@@ -63,9 +78,7 @@ void AddNode(int Row, int Col)
 	if (Row < (R - 1) && PArray[Row + 1][Col] == '.') { // Снизу есть путь
 		int DownGIndex = Maze[Index + C];
 		if (DownGIndex == -1) { // Поля снизу нет в графе, добавляем
-			Graph.push_back(Node);
-			DownGIndex = Graph.size() - 1;
-			Maze[Index + C] = DownGIndex;
+			AddGraphNode(Row + 1, Col, Index + C, &DownGIndex);
 		}
 		Graph[GIndex].push_back(DownGIndex); // Делаем ребро 
 		Graph[DownGIndex].push_back(GIndex);
@@ -89,7 +102,11 @@ int main()
 	}
 
 	AddNode(KR, KC);    // Начальную позицию в граф
-
+	for (int i = (KR < 2 ? 2 : KR) - 2; i < (KR + 3 > R ? R : KR + 3); i++) {
+		for (int j = (KC < 2 ? 2 : KC) - 2; j <(KC + 3 > C ? C : KC + 3); j++) {
+			if (Array[i][j] == '.') AddNode(i, j);
+		}
+	}
 						// game loop
 	while (1) {
 
